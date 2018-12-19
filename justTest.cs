891643728d7a852eba21424b4a62adca99cdf345
Jason Bakthakumar <jason@Jasons-Mac-mini.local>
2018-12-19 15:53:34 +1300

    Removed unneccessary code

--- justTest.cs
+++ justTest.cs
@@ -83,60 +83,6 @@ namespace justTest
 
 
 
-                /**
-                public static string Encrypt(string input, string key)  
-        {  
-            byte[] inputArray = UTF8Encoding.UTF8.GetBytes(input);  
-            TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();  
-            tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);  
-            tripleDES.Mode = CipherMode.ECB;  
-            tripleDES.Padding = PaddingMode.PKCS7;  
-            ICryptoTransform cTransform = tripleDES.CreateEncryptor();  
-            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);  
-            tripleDES.Clear();  
-            return Convert.ToBase64String(resultArray, 0, resultArray.Length);  
-        }  
-        public static string Decrypt(string input, string key)  
-        {  
-            byte[] inputArray = Convert.FromBase64String(input);  
-            TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();  
-            tripleDES.Key = UTF8Encoding.UTF8.GetBytes(key);  
-            tripleDES.Mode = CipherMode.ECB;  
-            tripleDES.Padding = PaddingMode.PKCS7;  
-            ICryptoTransform cTransform = tripleDES.CreateDecryptor();  
-            byte[] resultArray = cTransform.TransformFinalBlock(inputArray, 0, inputArray.Length);  
-            tripleDES.Clear();   
-            return UTF8Encoding.UTF8.GetString(resultArray);  
-        }  
-                
-                
-                
-                
-                
-                
-                
-                
-                 */
-
-            /* StringBuilder inputStringBuild = new StringBuilder(password);  
-            StringBuilder outputStringBuild = new StringBuilder(password.Length);  
-            char Textch;  
-            for (int i = 0; i < password.Length; i++)  
-            {  
-                Textch = inputStringBuild[i];  
-                Textch = (char)(Textch ^ key);  
-                outputStringBuild.Append(Textch);  
-            }  
-            string outputString =  outputStringBuild.ToString();  
-
-            return outputString != null
-                ? (ActionResult)new OkObjectResult("Output is " + outputString)
-                : new BadRequestObjectResult("Hey Something went wrong. ");
-
-            }
-            */
-
-
 
 
 
